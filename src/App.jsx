cd5bf9e37847feb6fab228f560dce82ec6f16748
TeslaSeven7 <alex.aubrier@epitech.eu>
2023-10-07 16:59:58 +0200

    Listing of Json format songs via Redux and Middleware

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import Playlist from './components/Playlist';
+
 function App() {
-  return <></>
+	return (
+		<>
+			<div className='min-h-screen bg-slate-900 pt-20 px-4'>
+				<div className='max-w-xl mx-auto'>
+					{/*PLAYER */}
+					<h1 className='text-slate-100 text-2xl text-center'>
+						PLAYER REACT REDUX
+					</h1>
+					<Playlist />
+				</div>
+			</div>
+			{/*PLAYERPANEL */}
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import { configureStore } from '@reduxjs/toolkit';
+import playlist from './features/playlist';
+export const store = configureStore({
+	reducer: { playlist },
+});
